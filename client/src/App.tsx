666e37ee5f23b271d0303daf8208ee6cd95d4b74
RFS <7137874+rfs85@users.noreply.github.com>
2025-05-11 13:03:40 +0100

    Refactor Toaster component to use default export and update import in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import Toaster from "@/components/ui/toaster";
 import { Helmet } from "react-helmet";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
